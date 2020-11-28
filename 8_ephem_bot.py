f5ac47b5de46ea7577f17512c54d47688de7c20f
mign0n <56875363+mign0n@users.noreply.github.com>
2020-11-28 19:17:23 +0500

    Fixed the following issues: - use the `getattr()` function to access `ephem` object attributes; - remove unneeded functions `print()`; - use unpack instead of accessing by index; - use exceptions to catch errors.

--- 8_ephem_bot.py
+++ 8_ephem_bot.py
@@ -19,55 +19,38 @@ from datetime import date
 import ephem
 import settings
 
-logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
+logging.basicConfig(format="%(name)s - %(levelname)s - %(message)s",
                     level=logging.INFO,
-                    filename='bot.log')
+                    filename="bot.log")
 
-DATE_TODAY = date.today().strftime('%Y/%m/%d')
+DATE_TODAY = date.today().strftime("%Y/%m/%d")
 
 
 def greet_user(update, context):
-    text = 'Вызван /start'
-    print(text)
+    text = "Вызван /start"
     update.message.reply_text(text)
 
 
 def talk_to_me(update, context):
     user_text = update.message.text
-    print(user_text)
     update.message.reply_text(user_text)
 
 
 def get_constelation(update, context):
     user_text = update.message.text
-    user_request = user_text.split()
-    print(user_request)
-    planet = user_request[-1].lower()
-    print(planet)
-    if planet == 'mercury':
-        p = ephem.Mercury(DATE_TODAY)
-    elif planet == 'venus':
-        p = ephem.Venus(DATE_TODAY)
-    elif planet == 'mars':
-        p = ephem.Mars(DATE_TODAY)
-    elif planet == 'jupiter':
-        p = ephem.Jupiter(DATE_TODAY)
-    elif planet == 'saturn':
-        p = ephem.Saturn(DATE_TODAY)
-    elif planet == 'uranus':
-        p = ephem.Uranus(DATE_TODAY)
-    elif planet == 'neptune':
-        p = ephem.Neptune(DATE_TODAY)
-    elif planet == 'pluto':
-        p = ephem.Pluto(DATE_TODAY)
-    else:
-        update.message.reply_text('I don\'t know this planet. Maybe you meant the Moon?')
-        planet = 'moon'
-        p = ephem.Moon(DATE_TODAY)
-
-    constellation = ephem.constellation(p)
-    print(constellation)
-    update.message.reply_text(f'Today {planet.capitalize()} is in the constellation of {constellation[-1]}.')
+    _, planet = user_text.split()
+    planet = planet.lower().capitalize()
+    try:
+        p = getattr(ephem, planet)(DATE_TODAY)
+    except AttributeError:
+        update.message.reply_text("I don't know this planet. Maybe you meant the Moon?")
+        planet = "Moon"
+        p = getattr(ephem, planet)(DATE_TODAY)
+    try:
+        _, constellation = ephem.constellation(p)
+        update.message.reply_text(f"Today {planet.capitalize()} is in the constellation of {constellation}.")
+    except TypeError:
+        update.message.reply_text("It's not a planet. Enter the name of a planet in the Solar System.")
 
 
 def main():
